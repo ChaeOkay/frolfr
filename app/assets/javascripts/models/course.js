076da394bc698e462d101c3c441aac99b5297158
Patrick Dougall <pdougall1@gmail.com>
2014-10-09 18:27:13 -0400

    flesh out: course resource

--- app/assets/javascripts/models/course.js
+++ app/assets/javascripts/models/course.js
@@ -1,9 +1,11 @@
 App.Course = DS.Model.extend({
-    city: DS.attr('string'),
-    state: DS.attr('string'),
-    country: DS.attr('string'),
-    name: DS.attr('string'),
-    status: DS.attr('string'),
-    location: DS.attr('string'),
-    roundsPlayed: DS.attr('number')
+  city: DS.attr('string'),
+  state: DS.attr('string'),
+  country: DS.attr('string'),
+  name: DS.attr('string'),
+  status: DS.attr('string'),
+  location: DS.attr('string'),
+  roundsPlayed: DS.attr('number'),
+  rounds: DS.hasMany('round', { async: true }),
+  holes: DS.hasMany('hole', { async: true })
 });
